28027f6adf8e86aeaf069b38c6e11e3270ebf6bf
miguelgargurevich <30358901+miguelgargurevich@users.noreply.github.com>
2023-03-22 15:21:16 -0500

    Add files via upload

---
+++ daily-scrum-tool/assets/data.js
@@ -0,0 +1,82 @@
+//{
+const squads =  [
+        {
+            "Trovadores":[
+                {
+                    "nombre":"Miguel",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Harry",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Billy",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Mariana",
+                    "rol":"Product Owner"
+                },
+                {
+                    "nombre":"Baudy",
+                    "rol":"Scrum Master"
+                },
+                {
+                    "nombre":"Marco",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Jorge E",
+                    "rol":"Arquitecto"
+                },
+                {
+                    "nombre":"Julio",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Jesus",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Jhony",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Anthony",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Jorge M",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Vicente",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Yahir",
+                    "rol":"Team Member"
+                }
+            ]
+        },
+        {
+            "Quipus":[
+                {
+                    "nombre":"Lyz",
+                    "rol":"Product Owner"
+                },
+                {
+                    "nombre":"Monica C",
+                    "rol":"Team Member"
+                },
+                {
+                    "nombre":"Jym",
+                    "rol":"Arquitecto"
+                }
+            ]
+        }
+        
+        
+    ]
+//}
